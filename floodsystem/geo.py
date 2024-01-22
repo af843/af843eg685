adf0056b84a7f4e35b485c6670c6025e83e94d17
Kane Gu <eg685@cam.ac.uk>
2024-01-22 15:42:15 +0000

    Task 1B with comment

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -13,6 +13,8 @@ from .utils import sorted_by_key  # noqa
 
 def stations_by_distance(stations, p):
     # this function calculates the distance of a station to a position with coordinate p
+    # haversine method is used to calculate distance here
+    # return a list of tuples (station_name,distance_to_p) and the list is sorted by distance
     list_station_distance = []
     for i in range(len(stations)):
         d = haversine((stations[i].coord[0],stations[i].coord[1]),p)
