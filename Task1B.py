92fbf0fb02f9b90680f55d8488f7d4c7da0c4684
Kane Gu <eg685@cam.ac.uk>
2024-01-22 15:41:16 +0000

    Final version of 1B

--- Task1B.py
+++ Task1B.py
@@ -1,12 +1,18 @@
 from floodsystem.geo import stations_by_distance
 from floodsystem.stationdata import build_station_list
 
+# get stations using build_station_list() from stationdata
 stations = build_station_list()
 
+# get distances of stations to p using stations_by_distance() from geo
 distance_of_station_to_cambridge = stations_by_distance(stations,(52.2053, 0.1218))
 
+# add town to result
 result = []
 for i in range(len(distance_of_station_to_cambridge)):
     result.append((distance_of_station_to_cambridge[i][0].name,distance_of_station_to_cambridge[i][0].town,distance_of_station_to_cambridge[i][1]))
 
-print(result)
+# print result
+print(result[:10])
+print("--------------")
+print(result[-10:])
--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -6,14 +6,16 @@ geographical data.
 
 """
 import math
+from haversine import haversine, Unit
 
 from .utils import sorted_by_key  # noqa
 
 
 def stations_by_distance(stations, p):
+    # this function calculates the distance of a station to a position with coordinate p
     list_station_distance = []
     for i in range(len(stations)):
-        d = 2*6.371e6*math.asin(math.sqrt(((math.sin((p[0]-stations[i].coord[0])/2))**2)+(math.cos(stations[i].coord[0]))*(math.cos(p[0]))*(((math.sin((p[1]-stations[i].coord[1])/2))**2))))
+        d = haversine((stations[i].coord[0],stations[i].coord[1]),p)
         list_station_distance.append((stations[i],d))
     return sorted_by_key(list_station_distance,1)
 
