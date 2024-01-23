2301ecb5bbabcf83edc6f6c6b2ffab415cf48c3b
Angus <af843@cam.ac.uk>
2024-01-23 13:26:32 +0000

    stations within radius test written, stations by river test started

---
+++ test_geo.py
@@ -0,0 +1,20 @@
+""""Tests for stations_within_radius"""
+from floodsystem.station import MonitoringStation
+
+
+def test_stations_within_radius():
+    from floodsystem.geo import stations_within_radius
+    from floodsystem.stationdata import build_station_list
+    """"check that the stations within a 100km radius of a few points is less than the total number of stations"""
+    stations = build_station_list()
+    assert len(stations_within_radius(stations, (52.2053, 0.1218), 100)) < len(stations) #output should be fewer than
+    assert len(set(stations_within_radius(stations, (52.2053, 0.1218), 100))) == len(stations_within_radius(stations, (52.2053, 0.1218), 100))
+    assert len(stations_within_radius(stations, (23.4567,-45.6789), 1000)) == 0 #middle of atlantic
+    assert len(stations_within_radius(stations, (23.4567,-45.6789), 100000)) == len(stations) #all stations should be withing 100,000km
+    assert type(stations_within_radius(stations, (52.2053, 0.1218), 100)) == list
+    assert type(stations_within_radius(stations, (52.2053, 0.1218), 100)[0]) == MonitoringStation
+
+def test_stations_by_river():
+    from floodsystem.geo import stations_by_river
+    from floodsystem.stationdata import build_station_list
+    
