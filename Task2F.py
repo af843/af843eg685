d7ced67e4bc74f1f8b592371094f6af81ac761ed
Kane Gu <eg685@cam.ac.uk>
2024-02-25 21:03:20 +0000

    Task2F

--- Task2F.py
+++ Task2F.py
@@ -11,8 +11,8 @@ from floodsystem.plot import plot_water_level_with_fit
 stations = build_station_list()
 update_water_levels(stations)
 
-data = stations_level_over_threshold(stations, 0)[0:5]
+data = stations_level_over_threshold(stations, 0)[0:1]
 dt = 10
 for i in data:
     dates,levels = fetch_measure_levels(i[0].measure_id,dt=datetime.timedelta(days=dt))
-    plot_water_level_with_fit(i[0], dates, levels, 4)
+    plot_water_level_with_fit(i[0], dates, levels, 5)
