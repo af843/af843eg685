6c5414d984f42b6e6cfb815d024b6632b6f7892a
Kane Gu <eg685@cam.ac.uk>
2024-02-27 15:38:56 +0000

    Final_with_test

--- test_warningissuer.py
+++ test_warningissuer.py
@@ -7,9 +7,10 @@ from floodsystem.flood import stations_level_over_threshold
 from floodsystem.datafetcher import fetch_measure_levels
 from floodsystem.stationdata import update_water_levels
 from floodsystem.plot import plot_water_level_with_fit
-from floodsystem.warningissuer import marking, predict_risk_level,get_relative_predicted_level,get_danger_level
+from floodsystem.warningissuer import predict_risk_level,get_relative_predicted_level,get_danger_level
 from floodsystem.analysis import polyfit
 from floodsystem.warningissuer import flood_predictor
+from floodsystem.warningissuer import warning 
 
 stations = build_station_list()
 update_water_levels(stations)
@@ -22,9 +23,21 @@ data = stations_level_over_threshold(stations, 0)[0][0]
 risk_stations = {}
 dates,levels = fetch_measure_levels(data.measure_id,dt=datetime.timedelta(days=10))
 predicted_level = predict_risk_level(data, dates, levels, 1, 2)
-risk_stations[data.name] = marking(get_relative_predicted_level(data,predicted_level))
+risk_stations[data.name] = get_relative_predicted_level(data,predicted_level)
 print(risk_stations)
 
 def test_flood_predictor():
-    assert flood_predictor([],0,0,0)
+    flood_predictor([],0,0,0)
+
+def test_get_relative_predicted_level():
+    get_relative_predicted_level([],None)
+
+def test_get_danger_level():
+    get_danger_level([],0,0,0)
+
+# def test_predict_risk_level():
+#     predict_risk_level([], 0, 0, 0, 0)
+
+def test_warning():
+    warning(None)
 
